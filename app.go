7aa7ba6f2f04b93a49e0c253ca4ea459d1dbe348
sert <demo.sert@gmail.com>
2017-02-18 23:13:59 +0900

    add comment

--- app.go
+++ app.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 )
 
+// 初期化処理
 func init() {
 	http.HandleFunc("/", handler)
 }
 
+// リクエスト受付関数
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, world!")
 }
